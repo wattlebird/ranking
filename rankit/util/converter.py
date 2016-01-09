a26cdcdbb05fc1709f5253a03b20a074c045079b
Ronnie WANG <geniusxiaoguai@gmail.com>
2016-01-09 20:54:05 +0800

    Added support for 'item_pair_rate' input table format.

--- rankit/util/converter.py
+++ rankit/util/converter.py
@@ -19,10 +19,10 @@ class Converter(object):
                 except KeyError:
                     raise KeyError("Failed to detect filetype.")
                 else:
-                    self.table = table
+                    self.table = table[['primary','secondary','rate1','rate2']]
                     self.filetype = "item_pair_rate"
             else:
-                self.table = table
+                self.table = table[['username','itemid','rate']]
                 self.filetype = "user_item_rate"
         elif filetype == 'user_item_rate':
             try:
@@ -30,7 +30,7 @@ class Converter(object):
             except:
                 raise
             else:
-                self.table = table
+                self.table = table[['username','itemid','rate']]
                 self.filetype = "user_item_rate"
         else:
             try:
@@ -38,7 +38,7 @@ class Converter(object):
             except:
                 raise
             else:
-                self.table = table
+                self.table = table[['primary','secondary','rate1','rate2']]
                 self.filetype = "item_pair_rate"
 
         self.itemlist = self._extract_list(self.table, self.filetype)
@@ -47,8 +47,19 @@ class Converter(object):
         if filetype == 'user_item_rate':
             iid = table.loc[:, 'itemid'].unique()
             item_id = pd.DataFrame({'itemid': iid,
-                                    'index': pd.Series(range(iid.shape[0]), dtype='int32')})
+                                    'index': pd.Series(range(iid.shape[0]),
+                                             dtype=np.int32)})
             return item_id
+        elif filetype == 'item_pair_rate':
+            iid = np.hstack([table.loc[:,'primary'].values,
+                  table.loc[:,'secondary'].values])
+            iid = np.unique(iid)
+            item_id = pd.DataFrame({'itemid': iid,
+                                    'index': pd.Series(range(iid.shape[0]),
+                                             dtype=np.int32)})
+            return item_id
+        else:
+            return None
 
     def colleymatrix(self):
         item_id = self.itemlist
@@ -80,6 +91,33 @@ class Converter(object):
                         else:
                             w[i1, :] += 0.5
                             w[i2, :] += 0.5
+
+            b = np.ravel(1 + 0.5 * (w[:, 0] - w[:, 1]))
+            for i in xrange(icnt):
+                C[i, i] = 2 + w[i, 0] + w[i, 1]
+            return (C, b)
+        elif self.filetype == "item_pair_rate":
+            idx = pd.DataFrame(item_id['index'].values, columns=['index'],
+                  index=item_id['itemid'])
+            icnt = item_id.shape[0]
+            # allocate space for computing
+            C = np.zeros((icnt, icnt), dtype=np.float32)
+            w = np.zeros((icnt, 2), dtype=np.float32)
+
+            for i in xrange(table.shape[0]):
+                i1 = idx.loc[table.loc[i,'primary'],'index']
+                i2 = idx.loc[table.loc[i,'secondary'],'index']
+                C[i1, i2] -= 1
+                C[i2, i1] -= 1
+                if table.loc[i,'rate1'] > table.loc[i,'rate2']:
+                    w[i1, 0] += 1
+                    w[i2, 1] += 1
+                elif table.loc[i,'rate1'] < table.loc[i,'rate2']:
+                    w[i1, 1] += 1
+                    w[i2, 0] += 1
+                else:
+                    w[i1, :] += 0.5
+                    w[i2, :] += 0.5
 
             b = np.ravel(1 + 0.5 * (w[:, 0] - w[:, 1]))
             for i in xrange(icnt):
