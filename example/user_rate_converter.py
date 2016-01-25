6daa6e1f84b59c6d45ab591edb48c4ceb54c26d3
Ronnie WANG <geniusxiaoguai@gmail.com>
2016-01-25 14:26:01 +0800

    Acclerated conversion process by cython

---
+++ example/user_rate_converter.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from util import fast_convert
+
+
+def user_rate_converter(filename, converttype='arithmetic_mean'):
+    """This function is designed to convert a user_item_rate dataframe to
+    item_pair_rate dataframe.
+    There are altogether four convert types:
+    'arithmetic_mean': one contest is the arithmetic mean of all watch records.
+    'log_mean': one record is the log geometric mean of all watch records.
+    'probability': compare by statistical frequency.
+    """
+
+    table = pd.read_hdf('Data/input.bin', 'user_item_rate')
+    table = table[['username', 'itemid', 'rate']]
+    table.sort_values(by=['username', 'itemid'], inplace=True)
+    iid = table.loc[:, 'itemid'].unique()
+    idx = dict(zip(iid, range(iid.shape[0])))
+    maxitem = len(idx)
+
+    matx = np.zeros((maxitem, maxitem), dtype=np.float32)
+    count = np.zeros((maxitem, maxitem), dtype=np.float32)
+
+    b = 0
+    for e in xrange(1, table.shape[0]+1):
+        if e==table.shape[0] or table.iloc[e, 0]!=table.iloc[b, 0]: # new block start
+            if e-b==1:
+                b=e;
+                continue;
+
+            temptable = table.iloc[b:e]
+            idxlst = np.zeros(e-b, dtype=np.int32)
+            for i in xrange(e-b):
+                idxlst[i] = idx[temptable.iloc[i, 1]]
+            ratelst = np.require(temptable.iloc[:, 2].values, dtype=np.float32)
+            if converttype=='arithmetic_mean':
+                operation=1
+            elif converttype=='log_mean':
+                operation=2
+            else:
+                operation=3
+            fast_convert(idxlst, ratelst, matx, count, operation)
+            b=e
+    count = np.triu(count+count.T, k=1)
+
+    # matrix is constructed. Now build the item pair table.
+    primary = []
+    secondary = []
+    rate1 = []
+    rate2 = []
+    for i in xrange(maxitem):
+        for j in xrange(i+1, maxitem):
+            if count[i, j]!=0:
+                primary.append(iid[i])
+                secondary.append(iid[j])
+                rate1.append(matx[i, j]/count[i, j])
+                rate2.append(matx[j, i]/count[i, j])
+    newtable = pd.DataFrame({'primary': primary,
+                             'secondary': secondary,
+                             'rate1': rate1,
+                             'rate2': rate2,
+                             'weight': np.ones(np.count_nonzero(count), dtype=np.float32)},
+                             columns = ['primary', 'secondary', 'rate1',
+                                        'rate2', 'weight'])
---
+++ example/util.pyx
@@ -0,0 +1,32 @@
+from __future__ import division
+import numpy as np
+cimport numpy as np
+ctypedef np.int32_t INDEXTYPE_t
+ctypedef np.float32_t RATETYPE_t
+from libc.math import log
+cimport cython
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def fast_convert(np.ndarray[INDEXTYPE_t, ndim=1] idxlst,
+                 np.ndarray[RATETYPE_t, ndim=1] ratelst,
+                 np.ndarray[RATETYPE_t, ndim=2] matx,
+                 np.ndarray[RATETYPE_t, ndim=2] count,
+                 int operation):
+    cdef unsigned int i, j;
+    cdef unsigned int ii, ij;
+    cdef unsigned int len = idxlst.shape[0];
+    for i in xrange(len):
+        for j in xrange(i+1, len):
+            ii = <unsigned int>(idxlst[i]);
+            ij = <unsigned int>(idxlst[j]);
+            count[ii, ij]+=1
+            if operation==1:
+                matx[ii, ij]+=ratelst[i]
+                matx[ij, ii]+=ratelst[j]
+            elif operation==2:
+                matx[ii, ij]+=log(ratelst[i])
+                matx[ij, ii]+=log(ratelst[j])
+            else:
+                matx[ii, ij]+=1 if ratelst[i]>ratelst[j] else 0
+                matx[ij, ii]+=1 if ratelst[i]<ratelst[j] else 0
