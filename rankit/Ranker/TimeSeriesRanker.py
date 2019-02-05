958720e3599aee1d60ee26613da5c6fa8463066d
Ronnie Wang <geniusxiaoguai@gmail.com>
2019-02-05 22:39:57 +0800

    Implemented Glicko 2

--- rankit/Ranker/TimeSeriesRanker.py
+++ rankit/Ranker/TimeSeriesRanker.py
@@ -4,10 +4,12 @@ import numpy as np
 import pandas as pd
 from rankit.Table import Table
 from scipy.stats import norm
+import math
 import warnings
 
 pdf = norm.pdf
 cdf = norm.cdf
+PI = math.pi
 
 class TimeSeriesRanker(object):
     """Base class for all time series ranking algorithms."""
@@ -220,3 +222,124 @@ class TrueSkillRanker(TimeSeriesRanker):
         }, columns=["name", "rating"])
         rtn['rank'] = rtn.rating.rank(method=method, ascending=False).astype(np.int32)
         return rtn.sort_values(by=['rating', 'name'], ascending=False).reset_index(drop=True)
+
+class GlickoRanker(TimeSeriesRanker):
+    def __init__(self, *, baseline = 1500, rd = 350, votality = 0.06, tau = 0.5, epsilon = 0.000001):
+        self.baseline = rating
+        self.rd = rd
+        self.votality = votality
+        self.tau = tau
+        self.epsilon = epsilon
+        self.data = Table()
+        self.miu = [] # normalized rating
+        self.phi = [] # normalized rd
+        self.sigma = [] # votality
+        # stateful variables, valid in each rating period
+        self.v = dict()
+        self.delta = dict()
+        
+    @staticmethod
+    def g(phi):
+        return 1/(1+3*phi**2/PI**2)**(1/2)
+    
+    @staticmethod
+    def E(miu, miut, phit):
+        return 1/(1+math.exp(g(phit)*(miut-miu)))
+
+    @staticmethod
+    def volatility_iter(deltaSqr, phiSqr, sigma, votality):
+        epsilon, tau = self.epsilon, self.tau
+        f = lambda x: math.exp(x) * (deltaSqr - phiSqr - votality - math.exp(x)) / 2 / (phiSqr + votality + math.exp(x))**2 - (x - math.log(sigma**2)) / tau**2
+        A = 2 * math.log(sigma)
+        if deltaSqr > phiSqr + votality:
+            B = math.log(deltaSqr - phiSqr - votality)
+        else:
+            k = 1
+            while f(2 * math.log(sigma) - k * tau) < 0:
+                k += 1
+            B = 2 * math.log(sigma) - k * tau
+        fA = f(A)
+        fB = f(B)
+        while abs(B-A) > epsilon:
+            C = A + (A-B)*fA(fB-fA)
+            fC = f(C)
+            if fC*fB<0:
+                A = B
+                fA = fB
+            else:
+                fA = fA/2
+            B = C
+            fB = fC
+        return math.exp(A/2)
+
+    def setup(self, itemRatingLut = dict(), itemPhiLut = dict(), itemSigmaLut = dict()):
+        # TODO: By using this function, users have to know exactly the existing user rating, RD and volatility, which implies that source of information comes from saved data.
+        # So there must be a model saving function.
+        if len(itemRatingLut) != 0:
+            # derive itemlut, indexlut and itemnum from itmScoreLut to setup self.data
+            itemnum = len(itemRatingLut)
+            itemlut = dict() # from item to index
+            indexlut = [] # from index to item
+            for i, itm in enumerate(itemRatingLut.items()):
+                k, v = itm
+                itemlut[i] = k
+                indexlut.append(k)
+                self.indexScoreLut.append(v)
+            self.data.setup(itemlut, indexlut, itemnum)
+        # Then setup self.miu, phi and sigma
+        self.miu = []*itemnum
+        self.phi = []*itemnum
+        self.sigma = []*itemnum
+        for itm in itemRatingLut.items():
+            k, rating = itm
+            i = itemlut[k]
+            self.miu[i] = rating
+            self.phi[i] = itemPhiLut[k]
+            self.sigma[i] = itemSigmaLut[k]
+
+    def update(self, table):
+        # check time info
+        if table.table.time.iloc[0] is None:
+            warnings.warn("The table to be updated misses time information. In that case, the whole table record will be updated in a single rating period.")
+        else if self.data.table.shape[0] > 0 and table.table.time.iloc[0] <= self.data.table.time.iloc[-1]:
+            warnings.warn("The table to be updated is recorded in a time before or equal to existing period. We could not update players' info in a delayed manner. The time sequence will be discarded.")
+        g, E, volatility_iter, drawMargin = GlickoRanker.g, GlickoRanker.E, GlickoRanker.volatility_iter, self.drawMargin
+        self.data.update(table)
+        if self.data.itemnum > len(self.miu) :
+            self.miu = self.miu[:] + [(self.baseline - 1500) / 173.7178] * (self.data.itemnum - len(self.miu))
+            self.phi = self.phi[:] + [self.rd / 173.7178] * (self.data.itemnum - len(self.phi))
+            self.sigma = self.sigma[:] + [self.tau] * (self.data.itemnum - len(self.sigma))
+        
+        mtx = pd.DataFrame(data={
+            'hidx': pd.concat([table.table.hidx, table.table.vidx]),
+            'vidx': pd.concat([table.table.vidx, table.table.hidx]),
+            'hscore': pd.concat([table.table.hscore, table.table.vscore]),
+            'vscore': pd.concat([table.table.vscore, table.table.hscore]),
+            'weight': pd.concat([table.table.weight, table.table.weight]),
+            'time': pd.concat([table.table.time, table.table.time])
+        }, columns = ['hidx', 'vidx', 'hscore', 'vscore', 'weight', 'time']).reset_index(drop=True).sort_values(by=['time', 'hidx'])
+        
+        for grp in mtx.groupby(by='time'):
+            cur_time, gp = grp
+            gx = dict()
+            ex = dict()
+            vx = dict()
+            for i in gp.hidx.unique():
+                gx[i] = g(self.phi[i])
+            for recs in gp.groupby(by='hidx'):
+                player, results = recs
+                vt = []
+                dt = []
+                for rec in results.itertuples(index=False):
+                    s = 0.5 if abs(rec.hscore - rec.vscore) <= drawMargin else (0 if rec.hscore < rec.vscore else 1)
+                    ex[(rec.hidx, rec.vidx)] = E(self.miu[rec.hidx], self.miu[rec.vidx], self.phi[rec.vidx])
+                    vt.append(gx[rec.vidx]**2 * ex[(rec.hidx, rec.vidx)] * (1 - ex[(rec.hidx, rec.vidx)]))
+                    dt.append(gx[rec.vidx] * (s - ex[(rec.hidx, rec.vidx)]))
+                v = 1/vt.sum()
+                delta = v*dt.sum()
+                sigma_ = volatility_iter(delta**2, self.phi[player]**2, self.sigma[player], self.votality)
+                phiSqr_ = (self.phi[player]**2 + sigma_**2)
+                self.phi[player] = 1/(1/phiSqr_ + 1/v)**(1/2)
+                self.miu[player] += self.phi[player]**2 * dt.sum()
+
+                
